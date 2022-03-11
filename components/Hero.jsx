4c1ccfa583a36edb8158bdb95883537e7f8a07c7
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-03-11 01:58:34 +0100

    hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,88 +1,3 @@
-[![Puppycat_pose](https://emoji.gg/assets/emoji/8285-puppycat-pose.png)](https://emoji.gg/emoji/8285-puppycat-pose)
-
-## Build OpenSea Blockchain Web 3.0 App with Next.js | Sanity.io | thirdweb
-
-#### [check the video tutorial here](https://youtu.be/x3eRXeMB-4k)
-
-<br>
-
-#### Check this other tutorial for more details on this <u>Topic</u>
-
-##### [Build your own NFT marketplace like OpenSea clone with solidity,openzeppelin and polygon (SUBSCRIBE)](https://youtu.be/7Q5E6RvLlUw)
-
-<br>
-
----
-
-<br>
-
-# Docs
-
-> **Blockchain development:** Smart Contracts, security, IDE, Truffle & hardhat, Etherscan, Dapp | **NFT:** ERC's ... [all here](./z_articles.md) ✋
-
-<br>
-<br>
-
-## Interesting articles & links:
-
-##### How to make money with NFT as a developer?
-
-[How to make money with NFT as a developer?](https://youtu.be/5_Wu_X4LnAw)
-
-<br>
-
-##### NFT Marketplaces
-
-> NFT marketplace rankings and analysis. Find non-fungible token trading volumes, number of traders per NFT marketplace and more key metrics.
-
-[NFT Marketplaces](https://dappradar.com/nft/marketplaces) ✋
-
-<br>
-
----
-
-<br>
-
-# Login:
-
-#### Login in Metamask : [metamask](https://metamask.io/)
-
-#### Login in ThirdWeb : [Thirdweb](https://bit.ly/3EJLftx)
-
-#### Login [Opensea](https://testnets.opensea.io/)
-
- <br>
-
----
-
-<br>
-<br>
-
-# 🌈
-
-## Hero component
-
-- Create the **Hero.js** inside the **components folder**, then import it inside the **pages/index**
-
-<br>
-
-##### [Hero styles](https://drive.google.com/drive/folders/1BS_tG_B7kH3pJVBu6KJiLgsk4T2gt9Jr)
-
-<br>
-
-#### So here there is nothing ordinary, only the tailwind part but if you know about css, you understand what is happeing.
-
-- for example: it has a position relative, it s relative also the wrapper, than the fontsize is at 46px etc
-
-<br>
-
-- this [rounded-full](https://tailwindcss.com/docs/border-radius) on the images is a **border-radius**
-
-<br>
-
-- **h-screen** height: 100vh;
-
-```javascript
 import React from 'react'
 
 //
@@ -92,13 +7,22 @@ const style = {
   wrapper: `relative`,
   container: `before:content-[''] before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('https://lh3.googleusercontent.com/ujepnqpnL0nDQIHsWxlCXzyw4pf01yjz1Jmb4kAQHumJAPrSEj0-e3ABMZlZ1HEpJoqwOcY_kgnuJGzfXbd2Tijri66GXUtfN2MXQA=s250')] before:bg-cover before:bg-center before:opacity-30 before:blur`,
   contentWrapper: `flex h-screen relative justify-center flex-wrap items-center`,
-  copyContainer: `w-1/2`,
+
+  //
+  copyContainer: `w-1/2    `,
+
+  //
   title: `relative text-white text-[46px] font-semibold`,
   description: `text-[#8a939b] container-[400px] text-2xl mt-[0.8rem] mb-[2.5rem]`,
   ctaContainer: `flex`,
   accentedButton: ` relative text-lg font-semibold px-12 py-4 bg-[#2181e2] rounded-lg mr-5 text-white hover:bg-[#42a0ff] cursor-pointer`,
   button: ` relative text-lg font-semibold px-12 py-4 bg-[#363840] rounded-lg mr-5 text-[#e4e8ea] hover:bg-[#4c505c] cursor-pointer`,
-  cardContainer: `rounded-[3rem]`,
+  //
+  //img large -----------
+  cardContainer: `rounded-[3rem] bg-cyan-500 shadow-lg shadow-cyan-500/50`,
+  //
+  //https://tailwindcss.com/docs/box-shadow-color
+  //
   infoContainer: `h-20 bg-[#313338] p-4 rounded-b-lg flex items-center text-white`,
   author: `flex flex-col justify-center ml-4`,
   name: ``,
@@ -125,13 +49,37 @@ const Hero = () => {
             </div>
           </div>
           <div className={style.cardContainer}>
+            {/* 
+              
+              Big image
+              
+              
+              */}
+            <img
+              className="rounded-t-lg"
+              src="https://lh3.googleusercontent.com/ujepnqpnL0nDQIHsWxlCXzyw4pf01yjz1Jmb4kAQHumJAPrSEj0-e3ABMZlZ1HEpJoqwOcY_kgnuJGzfXbd2Tijri66GXUtfN2MXQA=s550"
+              alt=""
+            />
             <div className={style.infoContainer}>
+              {/* 
+                
+                
+                img avatar 
+                
+                
+                */}
+
               <img
                 className="h-[2.25rem] rounded-full"
                 src="https://lh3.googleusercontent.com/qQj55gGIWmT1EnMmGQBNUpIaj0qTyg4YZSQ2ymJVvwr_mXXjuFiHJG9d3MRgj5DVgyLa69u8Tq9ijSm_stsph8YmIJlJQ1e7n6xj=s64"
                 alt=""
               />
-              // // // DESCRIPTION IMAGE with circle avatar //
+
+              {/* 
+              
+              
+              
+              */}
               <div className={style.author}>
                 <div className={style.name}>Jolly</div>
                 <a
@@ -150,39 +98,3 @@ const Hero = () => {
 }
 
 export default Hero
-```
-
-<br>
-<br>
-
-#### Now lets get rid of the ugly x direction bar from the bottom of the page
-
-[<img src="./z_img-read/hero-preview-bars.gif"/>]()
-
-- Go to the **styles** folder and inside the **globals.css**
-
-```css
-html,
-body {
-  background-color: #202225;
-  padding: 0;
-  margin: 0;
-
-  overflow-x: hidden; ✋
-}
-
-a {
-  color: inherit;
-  text-decoration: none;
-}
-
-* {
-  box-sizing: border-box;
-}
-```
-
-<br>
-
-#### He didnt add the overflow-x:hidden , but i had to because with the setting above, the bars remained.
-
-[<img src="./z_img-read/hero-preview-without-bars.gif"/>]()
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <>
       {' '}
       <Header />
+      <Hero />
     </>
   )
 }
